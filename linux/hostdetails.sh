888a8f2a1bcd31e8d79e1becbe1a05b1a183780d
tejareddy <107536552+tejareddy2440@users.noreply.github.com>
2022-11-03 19:21:45 +0530

    script to view server details

---
+++ linux/hostdetails.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Shell script to print host details
+
+echo -e " Host name is : \c " ; hostname
+echo -e " Memory details are : \c " ; free -h
+echo -e " Disk usage is : \c " ; du -h 
+echo -e " CPU details are : \c " ; lscpu
